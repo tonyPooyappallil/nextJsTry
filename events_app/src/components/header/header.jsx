1f82f25e8d9b79c06a201a0d74c38934e42913b5
Antony Chacko <antony101tony@gmail.com>
2023-01-31 16:53:56 +0530

    done

--- events_app/src/components/header/header.jsx
+++ events_app/src/components/header/header.jsx
@@ -4,7 +4,7 @@ export const Header = () => (
   <header>
     <nav>
       <img />
-      <Link href="">Home</Link>
+      <Link href="/">Home</Link>
       <Link href="/events">Events</Link>
       <Link href="/about-us">about Us</Link>
     </nav>
